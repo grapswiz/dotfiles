340b9efad1bc8d25b83e3361f7043a474b6fddb9
grapswiz <ineeza@gmail.com>
2020-08-27 17:48:00 +0900

    get files from gist
    mirroring file to gist

--- initialize-mac.sh
+++ initialize-mac.sh
@@ -3,3 +3,8 @@
 xcode-select --install
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+curl -fsSL https://gist.githubusercontent.com/grapswiz/60b8f20e661d04fbc9e1/raw/4e590c90910827472e75d011534673fba0b114b8/brewfile > ~/brewfile
+curl -fsSL https://gist.githubusercontent.com/grapswiz/60b8f20e661d04fbc9e1/raw/4e590c90910827472e75d011534673fba0b114b8/osx.sh > ~/osx.sh
+
+brew bundle
