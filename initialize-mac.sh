0cd5857f84ddda353a84736d5a5e37995a148186
grapswiz <ineeza@gmail.com>
2020-09-02 01:56:33 +0900

    extract krypton to initialize-mac.sh
    when the script invokes, does not have other files

--- initialize-mac.sh
+++ initialize-mac.sh
@@ -7,12 +7,18 @@ xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # ssh key
-./krypton.sh
+echo 'Install kr via homebrew'
+brew install 'kryptco/tap/kr'
+kr restart
+kr pair
+ssh me.krypt.co
+kr github
+kr codesign
+kr codesign test
 
 git clone git@github.com:grapswiz/dotfiles.git
 
-brew bundle
-
+cd dotfiles || return
 ./osx.sh
 ./alfred.sh
 ./visual-studio-code.sh
