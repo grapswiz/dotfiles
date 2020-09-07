97895bb6144179314b96f6fd805e53bbacf5c15a
grapswiz <ineeza@gmail.com>
2020-09-08 01:38:01 +0900

    feat(alfred): set hotkey to famililar one

--- alfred.sh
+++ alfred.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/env zsh
+
+open -a Alfred\ 4.app
+
+# hotkey: option + space
+defaults write com.runningwithcrayons.Alfred-Preferences hotkey.default -dict key -int 49 mod -int 524288 string Space
